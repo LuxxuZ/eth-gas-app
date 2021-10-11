866bc204ba4507f4a142afb7bea90a5ba4ac8515
Ricardo <ricardo.zsabo@gmail.com>
2021-10-10 23:02:28 -0430

    Reload Update

--- src/components/GasContainer/index.js
+++ src/components/GasContainer/index.js
@@ -1,15 +1,28 @@
 //import rabbitLogo from "../../images/rabbit-logo.svg";
 import { CgSpinner } from "react-icons/cg";
 
-export default function GasContainer({ speed, value, color, loading }) {
+export default function GasContainer({
+  speed,
+  value,
+  color,
+  firstLoading,
+  fetching,
+}) {
   return (
     <div className={`flex flex-col justify-center items-center text-c${color}`}>
       <div
         className={`shadow-csw bg-tcwhite border-4 border-bcwhite rounded-full w-72 h-72 flex items-center justify-center`}
       >
         <p className="text-6xl font-bold text-shadow-gsw">
-          {!loading ? value / 10 : <CgSpinner className="animate-spin-fast" />}
+          {!firstLoading ? (
+            value / 10
+          ) : (
+            <CgSpinner className="animate-spin-fast" />
+          )}
         </p>
+        <div className="absolute w-10 h-10 flex items-center justify-center pt-32 text-gray-400">
+          {fetching && <CgSpinner className="animate-spin-fast" />}
+        </div>
       </div>
       <p className="font-medium text-cxl text-shadow-2gsw">{speed}</p>
     </div>
